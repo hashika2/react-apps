3406d66f208c78022dbfcbcfbf1005c7528084ec
hashika <m.g.hashikamaduranga@gmail.com>
2019-11-11 22:15:26 +0530

    do some basic performance on react

---
+++ reactmanager/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  
+  return (
+    <div className="App">
+      <h1>React page</h1>
+      <label htmlFor="name">name</label>
+    </div>
+  );
+}
+
+export default App;
