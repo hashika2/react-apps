8ecab8d4cd991e021b5595071a713d61d73688a6
hashika <m.g.hashikamaduranga@gmail.com>
2019-12-01 22:13:18 +0530

    set Api

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import { importDeclaration } from '@babel/types';
 import SearchBar from './SearchBar';
+import axios from 'axios';
 
 class App extends React.Component{
 
-    onSearchSubmit(term){
-        console.log(term);
+    async onSearchSubmit(term){
+      const response = await axios.get('https://api.unsplash.com/search/photos',{
+            params:{ query:term },
+            headers:{
+                Authorization :'Client-ID ddf2190ccf17f5223950452c547e987db72a63b4b4a88d1be6f32aaa3cdc07b1'
+            }
+        });
+        console.log(response.data.results);
     }
     render(){
         return(
