df2ca0e3c64d879592dbd43721d21b1ca16eb641
hashika <m.g.hashikamaduranga@gmail.com>
2019-12-01 17:26:02 +0530

    set search bar

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -2,11 +2,19 @@ import React from 'react';
 import { importDeclaration } from '@babel/types';
 import SearchBar from './SearchBar';
 
-const App=()=>{
-    return(
-    <div className="ui container" style={{marginTop:'10px'}}>
-        <SearchBar/>
-    </div>)
+class App extends React.Component{
+
+    onSearchSubmit(term){
+        console.log(term);
+    }
+    render(){
+        return(
+            <div className="ui container" style={{marginTop:'10px'}}>
+                <SearchBar onSubmit={this.onSearchSubmit}/>
+                
+            </div>)
+    }
+   
 }
 
 export default App;
--- pics/src/components/SearchBar.js
+++ pics/src/components/SearchBar.js
@@ -1,12 +1,20 @@
 import React from 'react';
 
 class SearchBar extends React.Component{
+
+     state={
+         term:''
+     }
+     onFromSubmit=(event)=>{
+        event.preventDefault();
+        this.props.onSubmit(this.state.term);
+     }
     render(){
         return(
             <div className="ui segment">
-                <form className="ui form" action="text">
+                <form onSubmit={this.onFromSubmit} className="ui form" action="text">
                     <label>Image Search</label>
-                    <input type="text" name="" id=""/>
+                    <input type="text" name="" id="" value={this.state.term} onChange={e=>this.setState({term:e.target.value})}/>
                 </form>
             </div>
         )
