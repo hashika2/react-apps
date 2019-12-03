a1a87f1004544d6c29a787afe412096f242ff6c0
hashika <m.g.hashikamaduranga@gmail.com>
2019-12-03 17:12:18 +0530

    finish pics app show pictures proper way

--- pics/src/components/ImageList.js
+++ pics/src/components/ImageList.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import './imageList.css';
+import ImageCard from './ImageCard';
 
 const imageList=(props)=>{
-   const images= props.images.map(({description,id,urls})=>{
-        return <img alt={description} key={id} src={urls.regular}/>
+   const images= props.images.map((image)=>{
+        return <ImageCard key={image.id} image={image} />
     });
     return(
-        <div>{images}</div>
+        <div className="image-list">{images}</div>
     )
 }
 
