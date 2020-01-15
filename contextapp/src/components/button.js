87b0c51eb63db12730b5bf4ae19fee0910a48d12
hashika <m.g.hashikamaduranga@gmail.com>
2020-01-15 22:32:53 +0530

    create context object

--- contextapp/src/components/button.js
+++ contextapp/src/components/button.js
@@ -6,7 +6,7 @@ const contextType=LanguageContext;
 class Button extends React.Component{
     static contextType=LanguageContext;
     render(){
-        console.log(this.context)
+
         return(
             <button className="ui button primary">Submit</button>
         )
